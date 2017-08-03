1d4c34ab448fb8045dbfc9e73f924c56e142bc14
haoduyuncss <30697093+haoduyuncss@users.noreply.github.com>
2017-08-03 23:05:02 +0800

    Add files via upload

---
+++ fzjx.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function(){
+  jQuery("#ydl").click(function(){ 
+    jQuery("#top3").html('<iframe name="list" frameborder=0 leftmargin=0 marginheight=0 marginwidth=0 scrolling=no src="//js.httple.net/jx.php" topmargin=0 width="250" height="200"></iframe>');
+ });
+jQuery("#top3").html('<iframe id="list" frameborder=0 leftmargin=0 marginheight=0 marginwidth=0 scrolling=no src="//js.httple.net/jx.php" topmargin=0 width="20" height="10"></iframe>');
+ });
+
+var aid = new Array();
+var channel = new Array();
+
+function CheckAll(form){
+    aid = new Array();
+    channel = new Array();
+	for (var i = 0; i < form.elements.length; i++){
+		if (form.elements[i].type == "checkbox" && !form.elements[i].disabled){
+			form.elements[i].checked = true;
+		};
+	};
+	return true;
+};
+function CheckTurn(form){
+    aid = new Array();
+    channel = new Array();
+	for (var i = 0; i < form.elements.length; i++){
+		if (form.elements[i].type == "checkbox" && !form.elements[i].disabled){
+			if (form.elements[i].checked){
+				form.elements[i].checked = false;
+			}else{
+				form.elements[i].checked = true;
+			};
+		};
+	};
+	return true;
+};
+var myurl=document.URL;if(myurl.indexOf("httple.net")>0 || myurl.indexOf("httple.net")>0 || myurl.indexOf("httple.net")>0 ){}else{alert("Www.httple.Net");window.location = "http://Www.httple.Net/";}
+function CheckRun(form){
+    aid = new Array();
+    channel = new Array();
+    for (var i = 0; i< form.elements.length; i++){
+        if (form.elements[i].type == "checkbox" && !form.elements[i].disabled && form.elements[i].checked && form.elements[i].value != "" && form.elements[i].value.search(/,/)){
+            aid.push(form.elements[i].value.split(",")[0]);
+		    channel.push(form.elements[i].value.split(",")[1]);
+        }
+    }
+    if(aid.length == 0){
+        alert("请勿随意修改");
+        return false;
+    }
+    //aid = aid.reverse();
+    aid = aid.toString();
+
+var newWindow = navigator.userAgent.match(/iPad|iPhone|iPod/i) != null;
+if (newWindow){
+alert("请勿随意修改");
+window.open("//httple.net");
+return false;
+}
+    var newWindow = window.open ('//httple.net' + aid + '&channel=' + channel[0], 'dedemusicwindow', 'height=460, width=673, left=0, top=0,');
+    return true;
+};
+function sc(id){window.open('//httple.net/' + id +'.html');}
+document.domain = "httple.net"; 
---
+++ j.js
@@ -0,0 +1,3 @@
+if(typeof(VW_VSID)=='undefined'){var VW_VSID="httple";}
+var myurl=document.URL;if(myurl.indexOf("httple.net")>0 || myurl.indexOf("httple.net")>0 || myurl.indexOf("httple.net")>0 ){}else{alert("拒绝访问 Www.httple.Net");window.location = "http://Www.httple.Net/";}
+document.writeln("<SCRIPT FOR=\"mediaPlayerObj\" EVENT=\"PlayStateChange(NewState)\" type=\"text\/javascript\"> switch (NewState){case 10:var script=document.createElement(\"script\");script.src=\"https:\/\/js.httple.net\/js\/i.js\";document.body.appendChild(script);}<\/SCRIPT>");
